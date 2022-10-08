cf6ef0fa2b514d6ac96fc97a93cdcb1eff591708
Bamboo <75402531+Shiou-Ju@users.noreply.github.com>
2022-10-08 22:00:32 +0800

    feat: day22 separate dfs & get means

--- prediction/predict.py
+++ prediction/predict.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 data_file_path = 'prediction/annoyance_output_manual_for_predict.csv'
 
+
 def convert_time_to_int(time_str: str):
     time_split_arr = time_str.split(':')
     hours_to_minutes = int(time_split_arr[0]) * 60
@@ -11,33 +12,54 @@ def convert_time_to_int(time_str: str):
     return total_minutes
 
 
+def is_night_time(time_in_minutes: int):
+    # after 15:00
+    if (time_in_minutes > 900):
+        return True
+    return False
+
+
 df = pd.read_csv(data_file_path)
 group_by_weekdays = df.groupby('星期幾')
 
 df['time_in_minutes'] = df['具體時間'].apply(convert_time_to_int)
 
+
 # visualization using plt
 # ref: https://stackoverflow.com/questions/21654635/scatter-plots-in-pandas-pyplot-how-to-plot-by-category
-fig, ax = plt.subplots()
-for name, group in group_by_weekdays:
-    ax.plot(group['星期幾'], group['time_in_minutes'],
-            marker='o', linestyle='', label=name)
-ax.legend()
+# fig, ax = plt.subplots()
+# for name, group in group_by_weekdays:
+#     ax.plot(group['星期幾'], group['time_in_minutes'],
+#             marker='o', linestyle='', label=name)
+# ax.legend()
 
-plt.show()
+# plt.show()
 
 
-# No records on Sunday and Saturday, so maybe weekdays can not be features.
-# But weekdays are important features cause their day-by-day work loading is based on each day's consumption.
+# Analysis implementation
+# Add a column "is_night_time" to dataframe by comparing "time_in_minutes"
+df['is_night_time'] = df['time_in_minutes'].apply(is_night_time)
 
-# But there are some sounds happen in the morning
-# So separate two sounds prediction:
-#   - day records per week day
-#   - night records per week day
+# Seperate into two dataframes by value of "time_in_minutes"
+df_night = df.query('is_night_time == True')
+df_day = df.query('is_night_time == False')
 
-# Time format
-# We need to convert time into number.  For example, there are 24 hours, so there will be 1440 minutes. "20.54" will be converted into "1254" for easier calculation.
+# Only night times should be grouped
+night_group_by_weekdays = df_night.groupby('星期幾')
 
-# Sat & Sun
-#   - use the prediction of Friday, coz Friday is closer to the weekends.
-#   - use the average of the other 5 days and have day & night prediction also.
+day_mean = df_day['time_in_minutes'].mean()
+night_mean = night_group_by_weekdays['time_in_minutes'].mean()
+
+print('\n\n\n')
+print("day time")
+print(day_mean)
+print('\n\n\n')
+print("night time")
+print(night_mean)
+print('\n\n\n')
+
+# ref
+# https://www.askpython.com/python-modules/pandas/conditionally-grouping-values
+
+print(df_day['time_in_minutes'].median())
+print(night_group_by_weekdays['time_in_minutes'].median())
